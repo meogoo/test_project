035c212663283a4563f78e5101df93b84af629f7
meogoo <16691607@qq.com>
2015-11-09 15:24:50 +0800

    first cpp file

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author:meida
+    > Mail: meida@com 
+    > Created Time: Mon Nov  9 15:23:37 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+void main()
+{
+	printf(stdout, "hello world");
+}
